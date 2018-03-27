5ff0d775e5de40439ef452a76428a057f1a2e959
Matthew Lourenco <matthew.lourenco@ocsbstudent.ca>
2018-03-27 22:51:00 +0000

    Finished swift code

---
+++ main.swift
@@ -0,0 +1,105 @@
+//
+//  main.swift
+//  This program tests the vehicle class
+//
+//  Created by Matthew Lourenco on 27/03/18.
+//  Copyright © 2018 MTHS. All rights reserved.
+//
+
+public class Vehicle {
+	
+	private enum MyError : Error {
+    	case InvalidLicensePlateException(String)
+	}
+	
+	public enum Colours {
+		case WHITE
+		case GREY
+		case BLACK
+		case RED
+		case ORANGE
+		case YELLOW
+		case GREEN
+		case BLUE
+		case PURPLE
+	}
+	
+	var _licensePlate: String
+	var _colour: Colours
+	var _numberOfDoors: Int = 4
+	var _speed: Int = 0    	// km/h
+	var _maxSpeed: Int = 200	// km/h
+	
+	public init(licensePlate: String, colour: Colours) throws {
+		//Default Constructor for vehicle class
+		if licensePlate.count == 7 {
+			_licensePlate = licensePlate
+		} else {
+			throw MyError.InvalidLicensePlateException("License is invalid length")
+		}
+		_colour = colour;
+	}
+	
+	func getLicensePlate() -> String { return _licensePlate } //Getter
+	
+	func setLicensePlate(newPlate: String) throws {
+		//Setter
+		if newPlate.count == 7 {
+			_licensePlate = newPlate
+		} else {
+			throw MyError.InvalidLicensePlateException("License is invalid length")
+		}
+	}
+	
+	func getColour() -> Colours { return _colour } //Getter
+	
+	func setColour(newColour: Colours) {
+		//Setter
+		_colour = newColour
+		}
+	
+	func getNumberOfDoors() -> Int { return _numberOfDoors } //Getter
+	
+	func getSpeed() -> Int { return _speed } //Getter
+	
+	func getMaxSpeed() -> Int { return _maxSpeed } //Getter
+	
+	func accelerate(amount: Int) {
+		//Allows the vehicle to accelerate an amount
+		_speed += amount
+		if(_speed > _maxSpeed) {
+			_speed = _maxSpeed
+		}
+	}
+	
+	func brake(amount: Int) {
+		//Allows the vehicle to brake an amount
+		_speed -= amount
+		if(_speed < 0) {
+			_speed = 0
+		}
+	}
+	
+	func getStatus() -> String{
+		//Returns the fields of the class
+		return "License plate: \(_licensePlate)\nColour: \(_colour)\nNumber of doors: \(_numberOfDoors)\nSpeed: \(_speed)km/h\nMaximum speed: \(_maxSpeed)km/h";
+	}
+}
+
+print("Vehicle 1")
+let vehicle1: Vehicle = try Vehicle(licensePlate: "AAAA111", colour: Vehicle.Colours.GREY)
+print(vehicle1.getStatus())
+vehicle1.accelerate(amount: 5)
+print(vehicle1.getStatus())
+vehicle1.accelerate(amount: 20)
+print(vehicle1.getStatus())
+vehicle1.brake(amount: 30)
+print(vehicle1.getStatus())
+
+print("\nVehicle 2")
+let vehicle2: Vehicle = try Vehicle(licensePlate: "BBBB222", colour: Vehicle.Colours.BLUE)
+print(vehicle2.getStatus())
+try vehicle2.setLicensePlate(newPlate: "JAVAWNS")
+print(vehicle2.getStatus())
+vehicle2.setColour(newColour: Vehicle.Colours.RED)
+print(vehicle2.getStatus())
